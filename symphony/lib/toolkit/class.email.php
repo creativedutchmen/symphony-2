8c9da7dfa0a8d00506e8d2b59b4d62c98f087d49
Huib Keemink <hkeemink@gmail.com>
2010-11-18 19:34:05 +0100

    Documented class.email.php

--- symphony/lib/toolkit/class.email.php
+++ symphony/lib/toolkit/class.email.php
@@ -1,16 +1,26 @@
 <?php
 
-	// Email factory class.
-	// Returns the right gateway class when initialised
+	/**
+	 * @package toolkit
+	 */
 	
 	include_once(TOOLKIT . '/class.emailgatewaymanager.php');
 	
+	/**
+	 * The Email class is a factory class to make it possible to send emails using different gateways.
+	 */
 	Abstract class Email{
 		
 		private $gateway;
 	
-		// Will use the gateway set in the preferences when no gateway is set here.
-		// Setting this to a specific gateway should only be used if special features of that gateway are used.
+		/**
+		 * Returns the EmailGateway to send emails with.
+		 * Calling this function multiple times will return unique objects.
+		 * @param string $gateway
+		 * 	The name of the gateway to use. Please only supply if specific gateway functions are beeing used.
+		 *  If the gateway is not found, it will throw an EmailException
+		 * @return EmailGateway
+		 */
 		function create($gateway = null){
 			$email_gateway_manager = new EmailGatewayManager($this);
 			if($gateway){
@@ -19,7 +29,7 @@
 					return $email_gateway_manager->create($default_gateway);
 				}
 				else{
-					throw new EmailException('Can not find default gateway. Please check if the extension supplying the gateway is still installed, or change settings accordingly');
+					throw new EmailException('Can not find gateway. Please check if the extension supplying the gateway is still installed, or change settings accordingly');
 				}
 			}
 			else{
