67d1e79501b9c845e191ac7807fd2a0351782442
Huib Keemink <huib.keemink@creativedutchmen.com>
2010-11-24 01:45:32 +0100

    Added rough documentation.

--- symphony/lib/toolkit/email-gateways/email.smtp.php
+++ symphony/lib/toolkit/email-gateways/email.smtp.php
@@ -4,6 +4,15 @@
 	require_once(TOOLKIT . '/class.emailhelper.php');
 	require_once(TOOLKIT . '/class.smtp.php');
 	
+	/**
+	 * One of the two core email gateways.
+	 * Provides simple SMTP functionalities.
+	 * Supports AUTH LOGIN, SSL and TLS.
+	 *
+	 * @author Huib Keemink, Michael Eichelsdörfer
+	 * @todo document, test
+	 */
+	
 	Class SMTPGateway extends EmailGateway{
 		
 		protected $_SMTP;
